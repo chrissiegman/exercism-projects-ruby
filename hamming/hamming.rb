016a3d12c9892596a57b80301869386282eb4d21
chris <chrissiegman@yahoo.com>
2015-11-15 11:18:00 -0600

    Replace for loop with enumerable

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -5,7 +5,7 @@ class Hamming
   def self.compute(strand1, strand2)
     raise ArgumentError if strand1.length != strand2.length
     count = 0
-    for i in (0...strand1.length)
+    (0...strand1.length).each do |i|
       if strand1[i] != strand2[i]
         count += 1
       end
